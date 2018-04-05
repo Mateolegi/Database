9366b5fc1869fc8620dedb247686476f2a241575
Mateo Leal Giraldo <mateo.leal@quipux.com>
2018-04-05 16:52:05 -0500

    Se elimina clase innecesaria PersistenceFile

--- src/main/java/io/github/mateolegi/database/util/PersistenceFile.java
+++
@@ -1,8 +0,0 @@
-package io.github.mateolegi.database.util;
-
-import java.io.InputStream;
-
-public class PersistenceFile {
-
-	private InputStream schemaIS = this.getClass().getClassLoader().getResourceAsStream("META-INF/persistence.xml");
-}
