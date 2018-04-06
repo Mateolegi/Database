708f95e1208c5cd19ea6c4aff1da420455f669a7
Mateo Leal <mateolegi@gmail.com>
2018-04-06 00:19:23 -0500

    Actualización de encabezados con la licencia y adición de documentación

--- src/main/java/io/github/mateolegi/database/dao/impl/CrudImpl.java
+++ src/main/java/io/github/mateolegi/database/dao/impl/CrudImpl.java
@@ -1,5 +1,25 @@
-/**
+/*
+ * The MIT License
  * 
+ * Copyright (c) 2010-2018 Google, Inc. http://angularjs.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS I
+ * THE SOFTWARE.
  */
 package io.github.mateolegi.database.dao.impl;
 
@@ -19,11 +39,12 @@ import io.github.mateolegi.database.util.TransactionException;
 
 /**
  * Implementación de la capa de acceso a datos genérica
- * @author Mateo Leal
+ * @author <a href="https://mateolegi.github.io">Mateo Leal</a>
  * @param <E> Clase de la entidad
  */
 public class CrudImpl<E> implements Crud<E> {
 
+	/** Clase que representa la entidad que se mapea en las consultas */
 	private final Class<E> clazz;
 
 	public CrudImpl(Class<E> clazz) {
